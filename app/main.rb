5ce51bf93494de3b28e40acf525c38114ac6dd2f
Amir Rajan <ar@amirrajan.net>
2020-02-12 17:52:19 -0600

    Wired up slash of sword.

--- app/main.rb
+++ app/main.rb
@@ -4,48 +4,68 @@ class Game
   attr_gtk
 
   def defaults
-    state.player.tile_size   = 64
-    state.player.speed       = 3
-    state.player.x         ||= 640
-    state.player.y         ||= 360
-    state.player.dir_x     ||=  1
-    state.player.dir_y     ||= -1
-    state.player.is_moving ||= false
+    player.tile_size   = 64
+    player.speed       = 3
+    player.x         ||= 640
+    player.y         ||= 360
+    player.dir_x     ||=  1
+    player.dir_y     ||= -1
+    player.is_moving ||= false
     state.watch_list       ||= {}
     state.show_watch_list    = true if state.tick_count == 0
   end
 
   def horizontal_run
     tile_index = 0.frame_index(6, 3, true)
-    tile_index = 0 if !state.player.is_moving
+    tile_index = 0 if !player.is_moving
 
     {
-      x: state.player.x,
-      y: state.player.y,
-      w: state.player.tile_size,
-      h: state.player.tile_size,
+      x: player.x,
+      y: player.y,
+      w: player.tile_size,
+      h: player.tile_size,
       path: 'sprites/horizontal-run.png',
-      tile_x: 0 + (tile_index * state.player.tile_size),
+      tile_x: 0 + (tile_index * player.tile_size),
       tile_y: 0,
-      tile_w: state.player.tile_size,
-      tile_h: state.player.tile_size,
-      flip_horizontally: state.player.dir_x > 0
+      tile_w: player.tile_size,
+      tile_h: player.tile_size,
+      flip_horizontally: player.dir_x > 0
     }
   end
 
   def horizontal_stand
     {
-      x: state.player.x,
-      y: state.player.y,
-      w: state.player.tile_size,
-      h: state.player.tile_size,
+      x: player.x,
+      y: player.y,
+      w: player.tile_size,
+      h: player.tile_size,
       path: 'sprites/horizontal-stand.png',
-      flip_horizontally: state.player.dir_x > 0
+      flip_horizontally: player.dir_x > 0
+    }
+  end
+
+  def horizontal_slash
+    tile_index   = player.slash_at.frame_index(5, 3, false)
+    tile_index ||= 0
+
+    {
+      x: player.x,
+      y: player.y,
+      w: player.tile_size * 1.2,
+      h: player.tile_size * 1.2,
+      path: 'sprites/horizontal-slash.png',
+      tile_x: 0 + (tile_index * 32),
+      tile_y: 0,
+      tile_w: 30,
+      tile_h: 30,
+      flip_horizontally: player.dir_x > 0
     }
   end
 
   def render
-    if state.player.is_moving
+    if player.slash_at
+      outputs.sprites << horizontal_slash
+    elsif player.is_moving
       outputs.sprites << horizontal_run
     else
       outputs.sprites << horizontal_stand
@@ -53,32 +73,44 @@ class Game
   end
 
   def render_watch_list
-    return if !state.render_watch_list
+    return if !state.show_watch_list
     outputs.labels << state.watch_list.map.with_index do |(k, v), i|
        [30, 710 - i * 28, "#{k}: #{v || "(nil)"}"]
     end
   end
 
+  def b_down?
+    inputs.controller_one.key_down.a # classice usb gamepad: a button is actually the b button lol
+  end
+
   def input
+    # player movement
     if vector = inputs.directional_vector
-      state.player.x += vector.x * state.player.speed
-      state.player.y += vector.y * state.player.speed
+      player.x += vector.x * player.speed
+      player.y += vector.y * player.speed
     end
-    state.watch_list[:player_location] = [state.player.x, state.player.y]
+    player.slash_at = b_down? if b_down?
+    state.watch_list[:slash_at] = player.slash_at
   end
 
   def calc
     if vector = inputs.directional_vector
       state.debug_label = vector
-      state.player.dir_x = vector.x
-      state.player.dir_y = vector.y
-      state.player.is_moving = true
+      player.dir_x = vector.x
+      player.dir_y = vector.y
+      player.is_moving = true
     else
       state.debug_label = vector
-      state.player.is_moving = false
+      player.is_moving = false
     end
 
+    state.watch_list[:is_moving] = player.is_moving
     state.watch_list[:directional_vector] = inputs.directional_vector
+    state.watch_list[:slash_elapsed] = !player.slash_at || player.slash_at.elapsed?(15)
+
+    if player.slash_at && player.slash_at.elapsed?(15)
+      player.slash_at = nil
+    end
   end
 
   # source is at http://github.com/amirrajan/dragonruby-link-to-the-past
@@ -88,6 +120,10 @@ class Game
     render_watch_list
     input
     calc
+  end
+
+  def player
+    state.player
   end
 end
 
